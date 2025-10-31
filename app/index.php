dea7503fae0076a520840149a14fa2f5978b6eb7
MBA13042004 <benabdellah.mosab@etu.uae.ac.ma>
2025-10-31 11:46:37 +0100

    CI.yml

--- app/index.php
+++ app/index.php
@@ -1,4 +1,4 @@
 <?php
 echo "<h1>Bienvenue sur mon application PHP </h1>";
-echo "<p>Docker fonctionne parfaitement avec Apache et PHP 8.2 Ndaaaaa !</p>";
+echo "<p>Docker fonctionne parfaitement avec Apache et PHP 8.2 Ndaaaaa hhhh !</p>";
 ?>
